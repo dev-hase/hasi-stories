86f94f73ee6106b8ac4170e2b08a056a88c598ad
Daniel Schmidt <2wusel@web.de>
2022-03-03 21:40:33 +0100

    em and title

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
 
             <h1 class="pt-4 text-5xl md:text-6xl text-gray-200 font-black"><glitchi>{{article.title}}</glitchi></h1>
 
-            <div class="pt-4 pb-10 text-md md:text-xl text-gray-200 font-thinnest"><glitchi>{{article.title}}</glitchi></div>
+            <h2 class="pt-4 pb-10 text-md md:text-xl text-gray-200 font-thinnest"><glitchi>{{article.description}}</glitchi></h2>
 
         </div>
  
@@ -80,45 +80,6 @@ html {
     background-blend-mode: multiply;
 }
 
-.stories-mystic p {
-
-text-shadow: 0 0.1em 20px rgba(0, 0, 0, 1), 0.05em -0.03em 0 rgba(0, 0, 0, 1),
-    0.05em 0.005em 0 rgba(0, 0, 0, 1), 0em 0.08em 0 rgba(0, 0, 0, 1),
-    0.05em 0.08em 0 rgba(0, 0, 0, 1), 0px -0.03em 0 rgba(0, 0, 0, 1),
-    -0.03em -0.03em 0 rgba(0, 0, 0, 1), -0.03em 0.08em 0 rgba(0, 0, 0, 1), -0.03em 0 0 rgba(0, 0, 0, 1) !important;
-
-}
-
-.stories-mystic em{
-    background: #eee;
-    color: #831843;
-    padding: .25rem .5rem;
-    text-shadow: none;
-}
-
-.stories-fresh {
-    background-color: #077a5d;
-    background-image: url(/canvas.png);
-    background-size: 50rem;
-    background-blend-mode: multiply;
-}
-
-.stories-fresh p {
-
-text-shadow: 0 0.1em 20px rgba(0, 0, 0, 1), 0.05em -0.03em 0 rgba(0, 0, 0, 1),
-    0.05em 0.005em 0 rgba(0, 0, 0, 1), 0em 0.08em 0 rgba(0, 0, 0, 1),
-    0.05em 0.08em 0 rgba(0, 0, 0, 1), 0px -0.03em 0 rgba(0, 0, 0, 1),
-    -0.03em -0.03em 0 rgba(0, 0, 0, 1), -0.03em 0.08em 0 rgba(0, 0, 0, 1), -0.03em 0 0 rgba(0, 0, 0, 1) !important;
-
-}
-
-.stories-fresh em{
-    background: #eee;
-    color: #077a5d;
-    padding: .25rem .5rem;
-    text-shadow: none;
-
-}
 
 .stories-dark {
     background-color: #060219;
@@ -128,7 +89,15 @@ text-shadow: 0 0.1em 20px rgba(0, 0, 0, 1), 0.05em -0.03em 0 rgba(0, 0, 0, 1),
 
 }
 
-.stories-dark p {
+
+.stories-fresh {
+    background-color: #077a5d;
+    background-image: url(/canvas.png);
+    background-size: 50rem;
+    background-blend-mode: multiply;
+}
+
+.stories-fresh p, .stories-dark p, .stories-mystic p {
 
 text-shadow: 0 0.1em 20px rgba(0, 0, 0, 1), 0.05em -0.03em 0 rgba(0, 0, 0, 1),
     0.05em 0.005em 0 rgba(0, 0, 0, 1), 0em 0.08em 0 rgba(0, 0, 0, 1),
@@ -137,12 +106,21 @@ text-shadow: 0 0.1em 20px rgba(0, 0, 0, 1), 0.05em -0.03em 0 rgba(0, 0, 0, 1),
 
 }
 
-.stories-dark em{
-    background: #eee;
-    color: #060219;
-    padding: .25rem .5rem;
+.stories-fresh em, .stories-dark em, .stories-mystic em {
+    background: #f5fba5;
+    color: #000;
     text-shadow: none;
-
+    mix-blend-mode: screen;
+    font-size: 2rem;
+    border-radius: .125rem;
+    text-transform: uppercase;
+    font-family: "Luckiest Guy";
+    font-style: normal;
+    line-height: 3.5rem;
+    padding: .5rem;
+    padding-top: .75rem;
+    padding-bottom: 0;
+    letter-spacing: .125rem;
 }
 
 .storie {
